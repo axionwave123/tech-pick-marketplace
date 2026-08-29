4aeb68cfc7133d616f222055112f6ed06ab97434
axionwave123 <hartgreat12@gmail.com>
2026-08-29 20:06:53 +0100

    feat: permanent category images part2 (data URLs)

---
+++ src/lib/category-images/part2.ts
@@ -0,0 +1,6 @@
+export const imgs = {
+  watches: 'https://litter.catbox.moe/cy7vc3.jpg',
+  gaming: 'https://litter.catbox.moe/jie2hi.jpg',
+  tvs: 'https://litter.catbox.moe/haiq4t.jpg',
+  power: 'https://litter.catbox.moe/6l4947.jpg',
+} as const;
